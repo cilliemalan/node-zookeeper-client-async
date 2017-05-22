43a0ac4c5f34c31ae591889310777dad4065120a
Cillié Malan <cilliemalan@gmail.com>
2017-05-22 15:26:12 +0200

    Added example in docs

--- index.js
+++ index.js
@@ -95,6 +95,39 @@ class AsyncTransaction {
 
 /**
  * A zookeeper client that has methods that return promises.
+ * @example
+ * const zk = require('node-zookeeper-client-async');
+ * 
+ * const client = zk.createAsyncClient("127.0.0.1:2181");
+ * 
+ * // connect to the server
+ * await client.connectAsync();
+ * console.log('connected!');
+ * 
+ * // create a node
+ * const rootPath = await client.mkdirpAsync('/test');
+ * console.log(`created ${rootPath}`)
+ * 
+ * // add some ephemeral nodes
+ * await client.createAsync('/test/counter-', Buffer.from('first'), null, zk.CreateMode.EPHEMERAL_SEQUENTIAL);
+ * await client.createAsync('/test/counter-', Buffer.from('second'), null, zk.CreateMode.EPHEMERAL_SEQUENTIAL);
+ * 
+ * // list the nodes
+ * const nodes = await client.getChildrenAsync('/test');
+ * 
+ * // print stuff to console
+ * console.log(`${rootPath} has the children:`)
+ *     await Promise.all(nodes.map(async node => {
+ *     const data = await client.getDataAsync(`/test/${node}`);
+ *     console.log(`  ${node}: ${data.data}`);
+ * }));
+ * 
+ * // delete everything
+ * await client.rmrfAsync(rootPath);
+ * 
+ * // shut down
+ * await client.closeAsync();
+ * console.log('disconnected');
  */
 class AsyncClient {
 
